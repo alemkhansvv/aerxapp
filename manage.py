3014f21d4e751aba06065522b481f5ebafa1378b
alemkhan <alemkhansv@gmail.com>
2024-08-16 09:48:47 +0500

    Updated manage.py for Flask CLI

--- manage.py
+++ manage.py
@@ -1,24 +1,21 @@
+import os
 from flask import Flask
 from flask_migrate import Migrate
-from models import db
+from models import db  # Импортируем вашу базу данных из models.py
+from app import app  # Убедитесь, что вы правильно импортируете ваше приложение
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_uri'
+# Получаем значение DATABASE_URL из переменной окружения
+database_url = os.getenv('DATABASE_URL', 'sqlite:///instance/database.db')
+
+# Корректируем URL, если он начинается с postgres://
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    from flask.cli import with_appcontext
-    import click
-
-    @click.command(name='db_upgrade')
-    @with_appcontext
-    def db_upgrade():
-        """Миграция базы данных."""
-        from flask_migrate import upgrade
-        upgrade()
-
-    app.cli.add_command(db_upgrade)
     app.run()
