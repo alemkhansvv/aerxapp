016d9c2cde548d111ccf5974e976ac9d0c30a6b1
alemkhansvv <128310389+alemkhansvv@users.noreply.github.com>
2024-09-13 16:53:56 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,11 @@
 import os
 from flask import Flask
 from flask_migrate import Migrate
-from models import db  # Импортируем вашу базу данных из models.py
-from app import app  # Убедитесь, что вы правильно импортируете ваше приложение
+from models import db
+from app import app
 
-# Получаем значение DATABASE_URL из переменной окружения
 database_url = os.getenv('DATABASE_URL', 'sqlite:///instance/database.db')
 
-# Корректируем URL, если он начинается с postgres://
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
